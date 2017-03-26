b44d197580b041748d64145835d05f8cd17af695
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-03-25 18:54:52 -0700

    create fully connected forward and backprop algorithms

--- cs231n/classifiers/fc_net.py
+++ cs231n/classifiers/fc_net.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from cs231n.layers import *
 from cs231n.layer_utils import *
 
@@ -9,7 +8,7 @@ class TwoLayerNet(object):
   A two-layer fully-connected neural network with ReLU nonlinearity and
   softmax loss that uses a modular layer design. We assume an input dimension
   of D, a hidden dimension of H, and perform classification over C classes.
-  
+
   The architecure should be affine - relu - affine - softmax.
 
   Note that this class does not implement gradient descent; instead, it
@@ -19,7 +18,7 @@ class TwoLayerNet(object):
   The learnable parameters of the model are stored in the dictionary
   self.params that maps parameter names to numpy arrays.
   """
-  
+
   def __init__(self, input_dim=3*32*32, hidden_dim=100, num_classes=10,
                weight_scale=1e-3, reg=0.0):
     """
@@ -36,7 +35,8 @@ class TwoLayerNet(object):
     """
     self.params = {}
     self.reg = reg
-    
+
+
     ############################################################################
     # TODO: Initialize the weights and biases of the two-layer net. Weights    #
     # should be initialized from a Gaussian with standard deviation equal to   #
@@ -45,11 +45,14 @@ class TwoLayerNet(object):
     # weights and biases using the keys 'W1' and 'b1' and second layer weights #
     # and biases using the keys 'W2' and 'b2'.                                 #
     ############################################################################
-    pass
-    ############################################################################
-    #                             END OF YOUR CODE                             #
-    ############################################################################
 
+    scale1 = .010
+    scale2 = .010
+
+    self.params['W1'] = scale1 * np.random.randn(input_dim, hidden_dim)
+    self.params['W2'] = scale2 * np.random.randn(hidden_dim, num_classes)
+    self.params['b1'] = np.zeros(hidden_dim)
+    self.params['b2'] = np.zeros(num_classes)
 
   def loss(self, X, y=None):
     """
@@ -69,22 +72,27 @@ class TwoLayerNet(object):
     - loss: Scalar value giving the loss
     - grads: Dictionary with the same keys as self.params, mapping parameter
       names to gradients of the loss with respect to those parameters.
-    """  
+    """
     scores = None
     ############################################################################
     # TODO: Implement the forward pass for the two-layer net, computing the    #
     # class scores for X and storing them in the scores variable.              #
     ############################################################################
-    pass
-    ############################################################################
-    #                             END OF YOUR CODE                             #
-    ############################################################################
+
+    W1 = self.params['W1']
+    b1 = self.params['b1']
+    W2 = self.params['W2']
+    b2 = self.params['b2']
+
+    hidden, cache_hidden = affine_relu_forward(X, W1, b1)
+    scores, cache_scores = affine_forward(hidden, W2, b2)
 
     # If y is None then we are in test mode so just return scores
     if y is None:
-      return scores
-    
+        return scores
+
     loss, grads = 0, {}
+
     ############################################################################
     # TODO: Implement the backward pass for the two-layer net. Store the loss  #
     # in the loss variable and gradients in the grads dictionary. Compute data #
@@ -95,10 +103,23 @@ class TwoLayerNet(object):
     # automated tests, make sure that your L2 regularization includes a factor #
     # of 0.5 to simplify the expression for the gradient.                      #
     ############################################################################
-    pass
-    ############################################################################
-    #                             END OF YOUR CODE                             #
-    ############################################################################
+
+    # Loss Function
+    data_loss, dscores = softmax_loss(scores, y)
+    regu_loss = 0.5 * self.reg * (np.sum(W1 ** 2) + np.sum(W2 ** 2))
+    loss = data_loss + regu_loss
+
+    da1, dW2, db2 = affine_backward(dscores, cache_scores)
+    dX, dW1, db1 = affine_relu_backward(da1, cache_hidden)
+
+    grads['W2'] = dW2
+    grads['b2'] = db2
+    grads['W1'] = dW1
+    grads['b1'] = db1
+
+    # Add Regularizatoin
+    grads['W2'] += self.reg * W2
+    grads['W1'] += self.reg * W1
 
     return loss, grads
 
@@ -109,12 +130,12 @@ class FullyConnectedNet(object):
   ReLU nonlinearities, and a softmax loss function. This will also implement
   dropout and batch normalization as options. For a network with L layers,
   the architecture will be
-  
+
   {affine - [batch norm] - relu - [dropout]} x (L - 1) - affine - softmax
-  
+
   where batch normalization and dropout are optional, and the {...} block is
   repeated L - 1 times.
-  
+
   Similar to the TwoLayerNet above, learnable parameters are stored in the
   self.params dictionary and will be learned using the Solver class.
   """
@@ -124,7 +145,7 @@ class FullyConnectedNet(object):
                weight_scale=1e-2, dtype=np.float32, seed=None):
     """
     Initialize a new FullyConnectedNet.
-    
+
     Inputs:
     - hidden_dims: A list of integers giving the size of each hidden layer.
     - input_dim: An integer giving the size of the input.
@@ -174,7 +195,7 @@ class FullyConnectedNet(object):
       self.dropout_param = {'mode': 'train', 'p': dropout}
       if seed is not None:
         self.dropout_param['seed'] = seed
-    
+
     # With batch normalization we need to keep track of running means and
     # variances, so we need to pass a special bn_param object to each batch
     # normalization layer. You should pass self.bn_params[0] to the forward pass
@@ -183,7 +204,7 @@ class FullyConnectedNet(object):
     self.bn_params = []
     if self.use_batchnorm:
       self.bn_params = [{'mode': 'train'} for i in xrange(self.num_layers - 1)]
-    
+
     # Cast all parameters to the correct datatype
     for k, v in self.params.iteritems():
       self.params[k] = v.astype(dtype)
@@ -201,7 +222,7 @@ class FullyConnectedNet(object):
     # Set train/test mode for batchnorm params and dropout param since they
     # behave differently during training and testing.
     if self.dropout_param is not None:
-      self.dropout_param['mode'] = mode   
+      self.dropout_param['mode'] = mode
     if self.use_batchnorm:
       for bn_param in self.bn_params:
         bn_param[mode] = mode
--- cs231n/solver.py
+++ cs231n/solver.py
@@ -17,16 +17,16 @@ class Solver(object):
   model, dataset, and various optoins (learning rate, batch size, etc) to the
   constructor. You will then call the train() method to run the optimization
   procedure and train the model.
-  
+
   After the train() method returns, model.params will contain the parameters
   that performed best on the validation set over the course of training.
   In addition, the instance variable solver.loss_history will contain a list
   of all losses encountered during training and the instance variables
   solver.train_acc_history and solver.val_acc_history will be lists containing
   the accuracies of the model on the training and validation set at each epoch.
-  
+
   Example usage might look something like this:
-  
+
   data = {
     'X_train': # training data
     'y_train': # training labels
@@ -74,7 +74,7 @@ class Solver(object):
   def __init__(self, model, data, **kwargs):
     """
     Construct a new Solver instance.
-    
+
     Required arguments:
     - model: A model object conforming to the API described above
     - data: A dictionary of training and validation data with the following:
@@ -82,7 +82,7 @@ class Solver(object):
       'X_val': Array of shape (N_val, d_1, ..., d_k) giving validation images
       'y_train': Array of shape (N_train,) giving labels for training images
       'y_val': Array of shape (N_val,) giving labels for validation images
-      
+
     Optional arguments:
     - update_rule: A string giving the name of an update rule in optim.py.
       Default is 'sgd'.
@@ -105,7 +105,7 @@ class Solver(object):
     self.y_train = data['y_train']
     self.X_val = data['X_val']
     self.y_val = data['y_val']
-    
+
     # Unpack keyword arguments
     self.update_rule = kwargs.pop('update_rule', 'sgd')
     self.optim_config = kwargs.pop('optim_config', {})
@@ -118,13 +118,13 @@ class Solver(object):
 
     # Throw an error if there are extra keyword arguments
     if len(kwargs) > 0:
-      extra = ', '.join('"%s"' % k for k in kwargs.keys())
-      raise ValueError('Unrecognized arguments %s' % extra)
+      extra = ', '.join('"{}"'.format(k) for k in kwargs.keys())
+      raise ValueError('Unrecognized arguments {}'.format(extra))
 
     # Make sure the update rule exists, then replace the string
     # name with the actual function
     if not hasattr(optim, self.update_rule):
-      raise ValueError('Invalid update_rule "%s"' % self.update_rule)
+      raise ValueError('Invalid update_rule "{}"'.format(self.update_rule))
     self.update_rule = getattr(optim, self.update_rule)
 
     self._reset()
@@ -146,7 +146,7 @@ class Solver(object):
     # Make a deep copy of the optim_config for each parameter
     self.optim_configs = {}
     for p in self.model.params:
-      d = {k: v for k, v in self.optim_config.iteritems()}
+      d = {k: v for k, v in self.optim_config.items()}
       self.optim_configs[p] = d
 
 
@@ -166,7 +166,7 @@ class Solver(object):
     self.loss_history.append(loss)
 
     # Perform a parameter update
-    for p, w in self.model.params.iteritems():
+    for p, w in self.model.params.items():
       dw = grads[p]
       config = self.optim_configs[p]
       next_w, next_config = self.update_rule(w, dw, config)
@@ -177,7 +177,7 @@ class Solver(object):
   def check_accuracy(self, X, y, num_samples=None, batch_size=100):
     """
     Check accuracy of the model on the provided data.
-    
+
     Inputs:
     - X: Array of data, of shape (N, d_1, ..., d_k)
     - y: Array of labels, of shape (N,)
@@ -185,12 +185,12 @@ class Solver(object):
       on num_samples datapoints.
     - batch_size: Split X and y into batches of this size to avoid using too
       much memory.
-      
+
     Returns:
     - acc: Scalar giving the fraction of instances that were correctly
       classified by the model.
     """
-    
+
     # Maybe subsample the data
     N = X.shape[0]
     if num_samples is not None and N > num_samples:
@@ -203,8 +203,9 @@ class Solver(object):
     num_batches = N / batch_size
     if N % batch_size != 0:
       num_batches += 1
+    num_batches = int(num_batches)
     y_pred = []
-    for i in xrange(num_batches):
+    for i in range(num_batches):
       start = i * batch_size
       end = (i + 1) * batch_size
       scores = self.model.loss(X[start:end])
@@ -221,15 +222,15 @@ class Solver(object):
     """
     num_train = self.X_train.shape[0]
     iterations_per_epoch = max(num_train / self.batch_size, 1)
-    num_iterations = self.num_epochs * iterations_per_epoch
+    num_iterations = int(self.num_epochs * iterations_per_epoch)
 
-    for t in xrange(num_iterations):
+    for t in range(num_iterations):
       self._step()
 
       # Maybe print training loss
       if self.verbose and t % self.print_every == 0:
-        print '(Iteration %d / %d) loss: %f' % (
-               t + 1, num_iterations, self.loss_history[-1])
+        print('(Iteration {} / {}) loss: {:06.4f}'.format(
+               t + 1, num_iterations, self.loss_history[-1]))
 
       # At the end of every epoch, increment the epoch counter and decay the
       # learning rate.
@@ -251,14 +252,14 @@ class Solver(object):
         self.val_acc_history.append(val_acc)
 
         if self.verbose:
-          print '(Epoch %d / %d) train acc: %f; val_acc: %f' % (
-                 self.epoch, self.num_epochs, train_acc, val_acc)
+          print('(Epoch {:4.2f} / {:4.2f}) train acc: {:4.2f}%; val_acc: {:4.2f}%'.format(
+                 self.epoch, self.num_epochs, train_acc*100, val_acc*100))
 
         # Keep track of the best model
         if val_acc > self.best_val_acc:
           self.best_val_acc = val_acc
           self.best_params = {}
-          for k, v in self.model.params.iteritems():
+          for k, v in self.model.params.items():
             self.best_params[k] = v.copy()
 
     # At the end of training swap the best params into the model
